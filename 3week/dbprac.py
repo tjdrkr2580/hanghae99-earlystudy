0c74bfa38d611c670e89ba4ab399aca1dcca8128
tjdrkr2580 <tjdrkr2580@naver.com>
2022-12-05 02:08:13 +0900

    add example

--- 3week/dbprac.py
+++ 3week/dbprac.py
@@ -25,3 +25,20 @@ db.users.update_one({'name':'sunduk'},{'$set':{'age': 1.5}})
 
 # 데이터 삭제 delete | 조건
 db.users.delete_one({'name' : 'taehyun'})
+
+
+# 저장 - 예시
+doc = {'name':'bobby','age':21}
+db.users.insert_one(doc)
+
+# 한 개 찾기 - 예시
+user = db.users.find_one({'name':'bobby'})
+
+# 여러개 찾기 - 예시 ( _id 값은 제외하고 출력)
+all_users = list(db.users.find({},{'_id':False}))
+
+# 바꾸기 - 예시
+db.users.update_one({'name':'bobby'},{'$set':{'age':19}})
+
+# 지우기 - 예시
+db.users.delete_one({'name':'bobby'})
