b776ed6363d78a13c34ec5536f7d43ff9de88ab5
tjdrkr2580 <tjdrkr2580@naver.com>
2022-12-04 09:35:51 +0900

    fin py1-4

--- 3week/hello.py
+++ 3week/hello.py
@@ -1,1 +1,16 @@
-print("hello sparta!!!");
+# 문자열 길이 세기
+
+text = "text"
+result = len(text)
+print(result)
+
+text2 = "abcdefghijk"
+#print(text[3:])
+print(text[3:7])
+
+email = "abc@naver.com"
+print(email.split("@")[1].split('.')[0])
+
+print("sparta"[:3])
+
+print("02-123-1234"[:2])
